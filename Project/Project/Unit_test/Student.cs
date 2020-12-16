44b8cfc90bda7597155567618e5940eafcea6854
Balázs Koczur <balazs.koczur@stud.uni-corvinus.hu>
2020-12-16 15:47:54 +0100

    Unit_test

---
+++ Project/Project/Unit_test/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Project.Unit_test
+{
+    public class Student
+    {
+        public string NeptunCode;
+        public string Name;
+        public int Age;
+        public Student(string name, int age, string neptun)
+        {
+            Name = name;
+            Age = age;
+            NeptunCode = neptun;
+
+            string pattern = @"[(A-Z)|(0-9)]{6}";
+            Regex regex = new Regex(pattern);
+
+            if (regex.IsMatch(neptun) == false)
+                throw new ArgumentException();
+
+        }
+    }
+}
---
+++ Project/Project/Unit_test/StudentTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Project.Unit_test
+{
+    public class StudentTest
+    {
+        [Fact]
+        public void StudentTestSucces()
+        {
+            
+            string name = "Béla";
+            int age = 20;
+            string neptun = "B1TM4N";
+            Student student = new Student(name, age, neptun);
+            Assert.True(student.Name == "Béla");
+            Assert.True(student.Age == 20);
+            Assert.True(student.NeptunCode == "B1TM4N");
+        }
+        [Fact]
+        public void StudentTestException()
+        {
+            
+            Student student;
+            Assert.Throws<ArgumentException>(() => student = new Student("Cecil", 42, "Cecil.42"));
+        }
+    }
+}
